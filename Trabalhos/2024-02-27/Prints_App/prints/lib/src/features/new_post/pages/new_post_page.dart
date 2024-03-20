9f2aff9de7700c3b40f5c498f7bd7456113e9097
Juan Felipe Alves Flores <vilaJJ@users.noreply.github.com>
2024-03-19 21:49:04 -0300

    Prints-App: Criada página de novo post

--- Trabalhos/2024-02-27/Prints_App/prints/lib/src/features/new_post/pages/new_post_page.dart
+++ Trabalhos/2024-02-27/Prints_App/prints/lib/src/features/new_post/pages/new_post_page.dart
@@ -8,12 +8,42 @@ class NewPostPage extends StatefulWidget {
 }
 
 class _NewPostPageState extends State<NewPostPage> {
+  late String texto;
+
+  @override
+  void initState() {
+    super.initState();
+
+    texto = 'Galeria';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: const Center(
-        child: Text("Tela para realizar uma postagem", style: TextStyle(color: Colors.black)),
+      child: Center(
+        child: Column(
+          children: [
+            Expanded(
+              child: GridView.count(
+                crossAxisCount: 3,
+                children: List.generate(
+                  99, 
+                  (index) => InkWell(
+                    onTap: () {
+
+                    },
+                    child: Card(                    
+                      color: Colors.purple.shade900,
+                      child: Image.asset(
+                        "assets/feed/twice.png",
+                        fit: BoxFit.cover,
+                      )  
+                    ),
+                  )),
+              )
+            )
+        ])
       ),
     );
   }
